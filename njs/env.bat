d11cce5dd96182cd871799200805ad4ad18ce1b9
Brett Lawson <brett19@gmail.com>
2014-04-18 07:11:25 -0300

    Added code to explicitly disable MSVS for GYP.

--- njs/env.bat
+++ njs/env.bat
@@ -2,4 +2,16 @@ SET NJSVER=%1
 SET NJSTAG=%NJSVER%-%ARCH%
 SET NJSDIR=%~dp0..\njs-files\%NJSTAG%\
 
+IF "%MSVSVER%"=="none" GOTO noVs
+GOTO specificVs
+
+
+:specificVs
 SET GYP_MSVS_VERSION=%MSVSYEAR%
+GOTO End
+
+:noVs
+SET GYP_MSVS_VERSION=none
+GOTO End
+
+:End
