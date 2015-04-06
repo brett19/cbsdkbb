704bba6b53136bc989459e7ab94068b40c9b0066
Brett Lawson <brett19@gmail.com>
2015-04-06 10:16:07 -0300

    Update to latest Node.js, Libcouchbase and PHP versions.

--- setup_main.bat
+++ setup_main.bat
@@ -13,50 +13,36 @@ mkdir njs-files
 
 echo "downloading libcouchbase files..."
 if "%ARCH%"=="x86" (
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.3.0_x86_vc9.zip" -O src\lcb-2.3.0-vc9-x86.zip
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.3.0_x86_vc10.zip" -O src\lcb-2.3.0-vc10-x86.zip
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.3.0_x86_vc11.zip" -O src\lcb-2.3.0-vc11-x86.zip
-
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.0_x86_vc9.zip" -O src\lcb-2.4.0-vc9-x86.zip
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.0_x86_vc10.zip" -O src\lcb-2.4.0-vc10-x86.zip
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.0_x86_vc11.zip" -O src\lcb-2.4.0-vc11-x86.zip
+  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.8_x86_vc9.zip" -O src\lcb-2.4.8-vc9-x86.zip
+  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.8_x86_vc10.zip" -O src\lcb-2.4.8-vc10-x86.zip
+  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.8_x86_vc11.zip" -O src\lcb-2.4.8-vc11-x86.zip
 )
 if "%ARCH%"=="x64" (
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.3.0_amd64_vc9.zip" -O src\lcb-2.3.0-vc9-x64.zip
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.3.0_amd64_vc10.zip" -O src\lcb-2.3.0-vc10-x64.zip
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.3.0_amd64_vc11.zip" -O src\lcb-2.3.0-vc11-x64.zip
-
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.0_amd64_vc9.zip" -O src\lcb-2.4.0-vc9-x64.zip
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.0_amd64_vc10.zip" -O src\lcb-2.4.0-vc10-x64.zip
-  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.0_amd64_vc11.zip" -O src\lcb-2.4.0-vc11-x64.zip
+  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.8_amd64_vc9.zip" -O src\lcb-2.4.8-vc9-x64.zip
+  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.8_amd64_vc10.zip" -O src\lcb-2.4.8-vc10-x64.zip
+  tools\wget -nc "http://packages.couchbase.com/clients/c/libcouchbase-2.4.8_amd64_vc11.zip" -O src\lcb-2.4.8-vc11-x64.zip
 )
 
 
 echo "downloading nodejs files..."
 if "%ARCH%"=="x86" (
-  tools\wget -nc "http://nodejs.org/dist/v0.10.26/node.exe" -O src\node-0.10.26-x86.exe
-  tools\wget -nc "http://nodejs.org/dist/v0.11.9/node.exe" -O src\node-0.11.9-x86.exe
+  tools\wget -nc "http://nodejs.org/dist/v0.10.38/node.exe" -O src\node-0.10.38-x86.exe
+  tools\wget -nc "http://nodejs.org/dist/v0.12.2/node.exe" -O src\node-0.12.2-x86.exe
 )
 if "%ARCH%"=="x64" (
-  tools\wget -nc "http://nodejs.org/dist/v0.10.26/x64/node.exe" -O src\node-0.10.26-x64.exe
-  tools\wget -nc "http://nodejs.org/dist/v0.11.9/x64/node.exe" -O src\node-0.11.9-x64.exe
+  tools\wget -nc "http://nodejs.org/dist/v0.10.38/x64/node.exe" -O src\node-0.10.38-x64.exe
+  tools\wget -nc "http://nodejs.org/dist/v0.12.2/x64/node.exe" -O src\node-0.12.2-x64.exe
 )
-tools\node tools\get_npm.js 0.10.26 src\npm-0.10.26.zip
-tools\node tools\get_npm.js 0.11.9 src\npm-0.11.9.zip
+tools\node tools\get_npm.js 0.10.38 src\npm-0.10.38.zip
+tools\node tools\get_npm.js 0.12.2 src\npm-0.12.2.zip
 
 
 echo "downloading php files..."
-REM if "%ARCH%"=="x86" (
-REM   tools\wget -nc "http://windows.php.net/downloads/php-sdk/deps-5.5-vc11-x86.7z" -O src\php-deps-5.5-vc11-x86.7z
-REM )
-REM if "%ARCH%"=="x64" (
-REM   tools\wget -nc "http://windows.php.net/downloads/php-sdk/deps-5.5-vc11-x64.7z" -O src\php-deps-5.5-vc11-x64.7z
-REM )
 if "%ARCH%"=="x86" (
-  tools\wget -nc "http://php.net/get/php-5.3.27.tar.bz2/from/this/mirror" -O src\php-src-5.3.27.tar.bz2
-  tools\wget -nc "http://php.net/get/php-5.4.27.tar.bz2/from/this/mirror" -O src\php-src-5.4.27.tar.bz2
+  tools\wget -nc "http://php.net/get/php-5.4.39.tar.bz2/from/this/mirror" -O src\php-src-5.4.39.tar.bz2
 )
-tools\wget -nc "http://php.net/get/php-5.5.11.tar.bz2/from/this/mirror" -O src\php-src-5.5.11.tar.bz2
+tools\wget -nc "http://php.net/get/php-5.5.23.tar.bz2/from/this/mirror" -O src\php-src-5.5.23.tar.bz2
+tools\wget -nc "http://php.net/get/php-5.6.7.tar.bz2/from/this/mirror" -O src\php-src-5.6.7.tar.bz2
 tools\wget -nc "http://windows.php.net/downloads/php-sdk/php-sdk-binary-tools-20110915.zip" -O src\php-sdk.zip
 tools\wget -nc "https://phar.phpunit.de/phpunit.phar" -O src\php-phpunit.phar
 tools\wget -nc "http://phpdoc.org/phpDocumentor.phar" -O src\php-phpdoc.phar
@@ -67,84 +53,58 @@ tools\wget -nc "http://phpdoc.org/phpDocumentor.phar" -O src\php-phpdoc.phar
 
 echo "installing lcb files..."
 if "%ARCH%"=="x86" (
-  if not exist lcb-files\2.3.0-vc9-x86 (
-    tools\7za x -y -otmp\ src\lcb-2.3.0-vc9-x86.zip
-    move /Y tmp\libcouchbase-2.3.0_x86_vc9 lcb-files\2.3.0-vc9-x86
+  if not exist lcb-files\2.4.8-vc9-x86 (
+    tools\7za x -y -otmp\ src\lcb-2.4.8-vc9-x86.zip
+    move /Y tmp\libcouchbase-2.4.8_x86_vc9 lcb-files\2.4.8-vc9-x86
   )
-  if not exist lcb-files\2.3.0-vc10-x86 (
-    tools\7za x -y -otmp\ src\lcb-2.3.0-vc10-x86.zip
-    move /Y tmp\libcouchbase-2.3.0_x86_vc10 lcb-files\2.3.0-vc10-x86
+  if not exist lcb-files\2.4.8-vc10-x86 (
+    tools\7za x -y -otmp\ src\lcb-2.4.8-vc10-x86.zip
+    move /Y tmp\libcouchbase-2.4.8_x86_vc10 lcb-files\2.4.8-vc10-x86
   )
-  if not exist lcb-files\2.3.0-vc11-x86 (
-    tools\7za x -y -otmp\ src\lcb-2.3.0-vc11-x86.zip
-    move /Y tmp\libcouchbase-2.3.0_x86_vc11 lcb-files\2.3.0-vc11-x86
-  )
-
-  if not exist lcb-files\2.4.0-vc9-x86 (
-    tools\7za x -y -otmp\ src\lcb-2.4.0-vc9-x86.zip
-    move /Y tmp\libcouchbase-2.4.0_x86_vc9 lcb-files\2.4.0-vc9-x86
-  )
-  if not exist lcb-files\2.4.0-vc10-x86 (
-    tools\7za x -y -otmp\ src\lcb-2.4.0-vc10-x86.zip
-    move /Y tmp\libcouchbase-2.4.0_x86_vc10 lcb-files\2.4.0-vc10-x86
-  )
-  if not exist lcb-files\2.4.0-vc11-x86 (
-    tools\7za x -y -otmp\ src\lcb-2.4.0-vc11-x86.zip
-    move /Y tmp\libcouchbase-2.4.0_x86_vc11 lcb-files\2.4.0-vc11-x86
+  if not exist lcb-files\2.4.8-vc11-x86 (
+    tools\7za x -y -otmp\ src\lcb-2.4.8-vc11-x86.zip
+    move /Y tmp\libcouchbase-2.4.8_x86_vc11 lcb-files\2.4.8-vc11-x86
   )
 )
 if "%ARCH%"=="x64" (
-  if not exist lcb-files\2.3.0-vc9-x64 (
-    tools\7za x -y -otmp\ src\lcb-2.3.0-vc9-x64.zip
-    move /Y tmp\libcouchbase-2.3.0_amd64_vc9 lcb-files\2.3.0-vc9-x64
+  if not exist lcb-files\2.4.8-vc9-x64 (
+    tools\7za x -y -otmp\ src\lcb-2.4.8-vc9-x64.zip
+    move /Y tmp\libcouchbase-2.4.8_amd64_vc9 lcb-files\2.4.8-vc9-x64
   )
-  if not exist lcb-files\2.3.0-vc10-x64 (
-    tools\7za x -y -otmp\ src\lcb-2.3.0-vc10-x64.zip
-    move /Y tmp\libcouchbase-2.3.0_amd64_vc10 lcb-files\2.3.0-vc10-x64
+  if not exist lcb-files\2.4.8-vc10-x64 (
+    tools\7za x -y -otmp\ src\lcb-2.4.8-vc10-x64.zip
+    move /Y tmp\libcouchbase-2.4.8_amd64_vc10 lcb-files\2.4.8-vc10-x64
   )
-  if not exist lcb-files\2.3.0-vc11-x64 (
-    tools\7za x -y -otmp\ src\lcb-2.3.0-vc11-x64.zip
-    move /Y tmp\libcouchbase-2.3.0_amd64_vc11 lcb-files\2.3.0-vc11-x64
-  )
-
-  if not exist lcb-files\2.4.0-vc9-x64 (
-    tools\7za x -y -otmp\ src\lcb-2.4.0-vc9-x64.zip
-    move /Y tmp\libcouchbase-2.4.0_amd64_vc9 lcb-files\2.4.0-vc9-x64
-  )
-  if not exist lcb-files\2.4.0-vc10-x64 (
-    tools\7za x -y -otmp\ src\lcb-2.4.0-vc10-x64.zip
-    move /Y tmp\libcouchbase-2.4.0_amd64_vc10 lcb-files\2.4.0-vc10-x64
-  )
-  if not exist lcb-files\2.4.0-vc11-x64 (
-    tools\7za x -y -otmp\ src\lcb-2.4.0-vc11-x64.zip
-    move /Y tmp\libcouchbase-2.4.0_amd64_vc11 lcb-files\2.4.0-vc11-x64
+  if not exist lcb-files\2.4.8-vc11-x64 (
+    tools\7za x -y -otmp\ src\lcb-2.4.8-vc11-x64.zip
+    move /Y tmp\libcouchbase-2.4.8_amd64_vc11 lcb-files\2.4.8-vc11-x64
   )
 )
 
 
 echo "installing nodejs files..."
 IF "%ARCH%"=="x86" (
-  IF NOT EXIST njs-files\0.10.26-x86 (
-    mkdir njs-files\0.10.26-x86
-    copy /Y src\node-0.10.26-x86.exe njs-files\0.10.26-x86\node.exe
-    tools\7za x -y -onjs-files\0.10.26-x86\ src\npm-0.10.26.zip
+  IF NOT EXIST njs-files\0.10.38-x86 (
+    mkdir njs-files\0.10.38-x86
+    copy /Y src\node-0.10.38-x86.exe njs-files\0.10.38-x86\node.exe
+    tools\7za x -y -onjs-files\0.10.38-x86\ src\npm-0.10.38.zip
   )
-  IF NOT EXIST njs-files\0.11.9-x86 (
-    mkdir njs-files\0.11.9-x86
-    copy /Y src\node-0.11.9-x86.exe njs-files\0.11.9-x86\node.exe
-    tools\7za x -y -onjs-files\0.11.9-x86\ src\npm-0.11.9.zip
+  IF NOT EXIST njs-files\0.12.2-x86 (
+    mkdir njs-files\0.12.2-x86
+    copy /Y src\node-0.12.2-x86.exe njs-files\0.12.2-x86\node.exe
+    tools\7za x -y -onjs-files\0.12.2-x86\ src\npm-0.12.2.zip
   )
 )
 IF "%ARCH%"=="x64" (
-  IF NOT EXIST njs-files\0.10.26-x64 (
-    mkdir njs-files\0.10.26-x64
-    copy /Y src\node-0.10.26-x64.exe njs-files\0.10.26-x64\node.exe
-    tools\7za x -y -onjs-files\0.10.26-x64\ src\npm-0.10.26.zip
+  IF NOT EXIST njs-files\0.10.38-x64 (
+    mkdir njs-files\0.10.38-x64
+    copy /Y src\node-0.10.38-x64.exe njs-files\0.10.38-x64\node.exe
+    tools\7za x -y -onjs-files\0.10.38-x64\ src\npm-0.10.38.zip
   )
-  IF NOT EXIST njs-files\0.11.9-x64 (
-    mkdir njs-files\0.11.9-x64
-    copy /Y src\node-0.11.9-x64.exe njs-files\0.11.9-x64\node.exe
-    tools\7za x -y -onjs-files\0.11.9-x64\ src\npm-0.11.9.zip
+  IF NOT EXIST njs-files\0.12.2-x64 (
+    mkdir njs-files\0.12.2-x64
+    copy /Y src\node-0.12.2-x64.exe njs-files\0.12.2-x64\node.exe
+    tools\7za x -y -onjs-files\0.12.2-x64\ src\npm-0.12.2.zip
   )
 )
 
@@ -154,12 +114,13 @@ IF NOT EXIST php-files\sdk (
 )
 
 if "%ARCH%"=="x86" (
-  CALL setup_installphp.bat 5.5 5.5.11 vc11 x86
-  CALL setup_installphp.bat 5.4 5.4.27 vc9 x86
-  CALL setup_installphp.bat 5.3 5.3.27 vc9 x86
+  CALL setup_installphp.bat 5.6 5.6.7 vc9 x86
+  CALL setup_installphp.bat 5.5 5.5.23 vc11 x86
+  CALL setup_installphp.bat 5.4 5.4.39 vc9 x86
 )
 if "%ARCH%"=="x64" (
-  CALL setup_installphp.bat 5.5 5.5.11 vc11 x64
+  CALL setup_installphp.bat 5.6 5.6.7 vc9 x86
+  CALL setup_installphp.bat 5.5 5.5.23 vc11 x64
 )
 
 
