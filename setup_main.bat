830062ff54500fec0837bb969e128f0ae90474c3
Brett Lawson <brett19@gmail.com>
2015-04-07 09:46:46 -0300

    Fix windows PHP build platform.

--- setup_main.bat
+++ setup_main.bat
@@ -140,12 +140,12 @@ IF NOT EXIST php-files\sdk (
 )
 
 if "%ARCH%"=="x86" (
-  CALL setup_installphp.bat 5.6 5.6.7 vc9 x86
+  CALL setup_installphp.bat 5.6 5.6.7 vc11 x86
   CALL setup_installphp.bat 5.5 5.5.23 vc11 x86
   CALL setup_installphp.bat 5.4 5.4.39 vc9 x86
 )
 if "%ARCH%"=="x64" (
-  CALL setup_installphp.bat 5.6 5.6.7 vc9 x86
+  CALL setup_installphp.bat 5.6 5.6.7 vc11 x86
   CALL setup_installphp.bat 5.5 5.5.23 vc11 x64
 )
 
