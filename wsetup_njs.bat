746ca9821f400cd16333665340f7d3a6c1cc6ebc
Brett Lawson <brett19@gmail.com>
2015-09-18 14:43:28 -0300

    Use nvmw to download node on Windows.

--- wsetup_njs.bat
+++ wsetup_njs.bat
@@ -4,41 +4,31 @@ SET NJSMODE=%1
 SET NJSVER=%2
 SET ARCH=%3
 
-
-echo Downloading NJS %1% %2 %3
-
-if "%NJSMODE%"=="njs" (
-  if "%ARCH%"=="x86" (
-    tools\wget -nc "http://nodejs.org/dist/v%NJSVER%/node.exe" -O src\node-%NJSVER%-x86.exe
-  )
-  if "%ARCH%"=="x64" (
-    tools\wget -nc "http://nodejs.org/dist/v%NJSVER%/x64/node.exe" -O src\node-%NJSVER%-x64.exe
-  )
-)
-if "%NJSMODE%"=="iojs" (
-  tools\wget -nc "http://iojs.org/dist/v%NJSVER%/win-%ARCH%/iojs.exe" -O src\node-%NJSVER%-%ARCH%.exe
-)
-if "%NJSMODE%"=="njsx" (
-  tools\wget -nc "http://nodejs.org/dist/v%NJSVER%/win-%ARCH%/node.exe" -O src\node-%NJSVER%-%ARCH%.exe
-)
-
-
 echo Installing NJS %1 %2 %3
 
 SET NVMVER=v%NJSVER%
+SET NVMPATH=v%NJSVER%
 if "%NJSMODE%"=="iojs" (
   SET NVMVER=iojs-v%NJSVER%
+  SET NVMPATH=iojs/v%NJSVER%
 )
 
-IF NOT EXIST njs-files\%NJSVER%-%ARCH% (
-  mkdir njs-files\%NJSVER%-%ARCH%
-)
+SET NEEDINSTALL=0
 IF NOT EXIST njs-files\%NJSVER%-%ARCH%\node.exe (
-  copy /Y src\node-%NJSVER%-%ARCH%.exe njs-files\%NJSVER%-%ARCH%\node.exe
+  SET NEEDINSTALL=1
 )
 IF NOT EXIST njs-files\%NJSVER%-%ARCH%\npm.cmd (
+  SET NEEDINSTALL=1
+)
+
+IF "%NEEDINSTALL%"="1" (
+  rmdir /S /Q njs-files\%NJSVER%-%ARCH%
+
   tools\nvmw\nvmw install %NVMVER%
-  move /Y tools\nvmw\%NVMVER%\node_modules njs-files\%NJSVER%-%ARCH%\node_modules
-  copy /Y tools\nvmw\%NVMVER%\npm.cmd njs-files\%NJSVER%-%ARCH%\npm.cmd
-  rmdir /S /Q tools\nvmw\%NVMVER%
+  del /Q tools\nvmw\%NVMPATH%\npm.zip
+
+  move /Y tools\nvmw\%NVMPATH% njs-files\%NJSVER%-%ARCH%
+
+  rmdir /S /Q tools\nvmw\%NVMPATH%
+  rmdir /S /Q tools\nvmw\iojs
 )
